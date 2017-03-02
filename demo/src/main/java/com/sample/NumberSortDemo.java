f9e4d6e3277be023a44a05c575dce18cce4fe45e
WanChongYang <happywuya054@163.com>
2017-03-02 17:30:11 +0800

    udp demo

--- demo/src/main/java/com/sample/NumberSortDemo.java
+++ demo/src/main/java/com/sample/NumberSortDemo.java
@@ -10,19 +10,19 @@ public class NumberSortDemo {
         int[] nums = {12, 4, 9, 10, 23, 1};
 
         //冒泡排序
-        NumberSortUtil.bubbleSort(nums);
+        //NumberSortUtil.bubbleSort(nums);
 
         //选择排序
-        NumberSortUtil.selectSort(nums);
+        //NumberSortUtil.selectSort(nums);
 
         //快速排序
         NumberSortUtil.quickSort(nums, 1, 3);
 
         //插入排序
-        NumberSortUtil.insertSort(nums);
+        //NumberSortUtil.insertSort(nums);
 
         //归并排序
-        NumberSortUtil.mergeSort(nums, 1, 5);
+        //NumberSortUtil.mergeSort(nums, 1, 5);
 
         for(int num : nums) {
             System.out.print(num + "\t");
