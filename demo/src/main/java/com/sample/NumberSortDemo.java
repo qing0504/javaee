1950e39f3ef05a67c3f93aa0e1ff99ef557a7e86
WanChongYang <happywuya054@163.com>
2017-03-02 17:21:45 +0800

    number sort demo

---
+++ demo/src/main/java/com/sample/NumberSortDemo.java
@@ -0,0 +1,31 @@
+package com.sample;
+
+import com.common.utils.NumberSortUtil;
+
+/**
+ * Created by Martin on 2017/3/2.
+ */
+public class NumberSortDemo {
+    public static void main(String[] args) {
+        int[] nums = {12, 4, 9, 10, 23, 1};
+
+        //冒泡排序
+        NumberSortUtil.bubbleSort(nums);
+
+        //选择排序
+        NumberSortUtil.selectSort(nums);
+
+        //快速排序
+        NumberSortUtil.quickSort(nums, 1, 3);
+
+        //插入排序
+        NumberSortUtil.insertSort(nums);
+
+        //归并排序
+        NumberSortUtil.mergeSort(nums, 1, 5);
+
+        for(int num : nums) {
+            System.out.print(num + "\t");
+        }
+    }
+}
