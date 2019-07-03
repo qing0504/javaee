503c8260d1ea97db6c518c5d19cdb1c6e7688312
wanchongyang <chongyang.wan@guanaitong.com>
2019-07-03 23:26:19 +0800

    java11 Duration

--- src/main/java/com/java8/time/DurationTest.java
+++ src/main/java/com/java8/time/DurationTest.java
@@ -1,5 +1,6 @@
 package com.java8.time;
 
+import java.text.DecimalFormat;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -9,17 +10,26 @@ import java.time.format.DateTimeFormatter;
  * @date 2019-06-28 12:06
  */
 public class DurationTest {
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("00");
+
     public static void main(String[] args) {
         DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime end = LocalDateTime.now();
         System.out.println("end :" + end.format(df));
-        LocalDateTime start = LocalDateTime.parse("2019-06-27 10:20:30", df);
+        LocalDateTime start = LocalDateTime.parse("2019-06-30 10:20:30", df);
         System.out.println("start :" + start.format(df));
 
         Duration duration = Duration.between(start, end);
+
         System.out.println("天：" + duration.toDays());
         System.out.println("时：" + (duration.toHours() - duration.toDays()*24));
         System.out.println("分：" + (duration.toMinutes() - duration.toHours()*60));
-        System.out.println("秒：" + (duration.getSeconds() - duration.toMinutes()*60));
+        System.out.println("秒：" + (duration.toSeconds() - duration.toMinutes()*60));
+
+        System.out.println("=====================");
+        System.out.println("天：" + DECIMAL_FORMAT.format(duration.toDaysPart()));
+        System.out.println("时：" + DECIMAL_FORMAT.format(duration.toHoursPart()));
+        System.out.println("分：" + DECIMAL_FORMAT.format(duration.toMinutesPart()));
+        System.out.println("秒：" + DECIMAL_FORMAT.format(duration.toSecondsPart()));
     }
 }
--- src/test/java/TestSample.java
+++ src/test/java/TestSample.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * @author wanchongyang
  * @date 2018/7/21
@@ -7,6 +9,8 @@ import org.junit.Test;
 public class TestSample {
     @Test
     public void test() {
-
+        for (int i = 0; i < 100; i++) {
+            System.out.println(ThreadLocalRandom.current().nextLong(100000L));
+        }
     }
 }
